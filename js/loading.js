a72035f241fb483a092f4f4e5c621ff649d449ee
h-mamedana <100557766+h-mamedana@users.noreply.github.com>
2022-03-04 23:38:39 +0900

    Add files via upload

--- js/loading.js
+++ js/loading.js
@@ -1,7 +1,7 @@
 window.setTimeout(function() {
     const spinner = document.getElementById('loading');
     spinner.classList.add('loaded');
-}, 1500);
+}, 1200);
 
 window.onpageshow = function (event) {
     if (event.persisted) {
